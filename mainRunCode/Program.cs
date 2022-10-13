5fe4b9f715d714e07c31d4646001ade8404bc409
Roman Magafurov <roman.mag1997@yandex.ru>
2022-10-13 17:13:58 +0500

    updated MainRunCode

--- mainRunCode/Program.cs
+++ mainRunCode/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] massivArray = new string[] {"tray", "fra", "ra", "2f2", "f4pi", "white", "red", "write", "line", "no", "yes", "false", ""};
+string strList = string.Empty;
+for (int i = 0; i < massivArray.Length; i++)
+{
+    if (Convert.ToString(massivArray[i]).Length < 4 &&
+                          massivArray[i] != string.Empty)
+    {
+        if (strList.Length == 0)
+        {
+            strList += Convert.ToString(massivArray[i]);
+        }
+        else
+        {
+            if (i < massivArray.Length)
+            {
+                strList += "\n";
+            }
+            strList += Convert.ToString(massivArray[i]);
+        }
+    }
+}
+string[] arrayList = new string[strList.Length];
+arrayList = strList.Split("\n");
+
+
+for (int i = 0; i < arrayList.Length; i++)
+{
+    Console.WriteLine(arrayList[i]);
+}
